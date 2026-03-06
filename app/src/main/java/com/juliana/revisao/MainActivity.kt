c2aaf0f5cd933bf4da800ac69f50b6b59a6c3cd9
jujubc <jubsbcorrea@gmail.com>
2026-03-06 15:33:46 -0300

    Exercício Revisão Concluído - 06/03/2026 Juliana B. Corrêa

--- app/src/main/java/com/juliana/revisao/MainActivity.kt
+++ app/src/main/java/com/juliana/revisao/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.juliana.revisao
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.juliana.revisao.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            val login = binding.loginInput.text.toString().trim()
+            val senha = binding.senhaInput.text.toString().trim()
+
+            if (login == "admin" && senha == "1234") {
+                val intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra("LOGIN_USUARIO", login)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Login ou senha incorretos!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/juliana/revisao/MainActivity2.kt
+++ app/src/main/java/com/juliana/revisao/MainActivity2.kt
@@ -5,12 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.juliana.revisao.databinding.ActivityMain2Binding
+import com.juliana.revisao.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val loginUsuario = intent.getStringExtra("LOGIN_USUARIO") ?: "Desconhecido"
+        binding.txtUsuario.text = "Bem-Vindo, $loginUsuario"
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
